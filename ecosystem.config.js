d5cc425ddeaa94e656cc31cc7add4d95a0f844ea
Razi Fauzi <razicharlie@gmail.com>
2024-12-19 01:53:16 +0800

    add pm2 ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  apps : [{
+    script: 'npm start'
+  }],
+
+  deploy : {
+    production : {
+      key: 'key.pem',
+      user : 'ubuntu',
+      host : '43.216.252.56',
+      ref  : 'origin/main',
+      repo : 'https://github.com/razifauzi/frontend.git',
+      path : '/home/ubuntu',
+      'pre-deploy-local': '',
+      'post-deploy' : 'soruce ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': '',
+      'ssh_options': 'ForwardAgent=yes'
+
+    }
+  }
+};
