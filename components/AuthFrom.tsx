a8a29e5facb55f1d448e6ae62be8e323880ac6df
Razi Fauzi <razicharlie@gmail.com>
2024-12-19 23:17:24 +0800

    change error

--- components/AuthFrom.tsx
+++ components/AuthFrom.tsx
@@ -12,15 +12,13 @@ import {Form} from "@/components/ui/form"
 import CustomInput from './CustomInput'
 import { authFormSchema } from '@/lib/utils'
 import { Loader2 } from 'lucide-react'
-import { useRouter } from 'next/navigation'
-import {  signIn, signUp } from '@/lib/actions/user.actions'
+
 
 
 const AuthFrom = ({type}:{type:string}) => {
 
-const router = useRouter()
-const [user,setUser] = useState(null)
-const [isLoading,setIsLoading] = useState(false)
+const [user] = useState(null)
+const [isLoading] = useState(false)
 const formSchema = authFormSchema(type)
 
   const form = useForm<z.infer<typeof formSchema>>({
@@ -31,28 +29,28 @@ const formSchema = authFormSchema(type)
     },
   })
   
-  const onSubmit = async (data: z.infer<typeof formSchema>) => {
-    setIsLoading(true)
-    try{
-      if(type === 'sign-up'){
-        const newUser = await signUp(data)
-        setUser(newUser)
-      }
-      if(type === 'sign-in'){
-        const response = await signIn(
-          {
-            email: data.email,
-            password: data.password,
-          }
-        )
-        if(response) router.push('/')
-      }
-    }catch (error){
-      console.log(error)
-    } finally {
-      setIsLoading(false)
-    }
-  }
+  // const onSubmit = async (data: z.infer<typeof formSchema>) => {
+  //   setIsLoading(true)
+  //   try{
+  //     if(type === 'sign-up'){
+  //       // const newUser = await signUp(data)
+  //       // setUser(newUser)
+  //     }
+  //     if(type === 'sign-in'){
+  //       // const response = await signIn(
+  //       //   {
+  //       //     email: data.email,
+  //       //     password: data.password,
+  //       //   }
+  //       // )
+  //       // if(response) router.push('/')
+  //     }
+  //   }catch (error){
+  //     console.log(error)
+  //   } finally {
+  //     setIsLoading(false)
+  //   }
+  // }
   return (
    <section className='auth-form'>
       <header className='flex flex-col gap-5 md:gap-8'>
@@ -91,7 +89,7 @@ const formSchema = authFormSchema(type)
       ):(
         <>
           <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <form  className="space-y-8">
               {type === 'sign-up' && (
                 <>
                   <div className="flex gap-4">
